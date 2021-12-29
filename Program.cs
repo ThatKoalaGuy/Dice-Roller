f53debeb7ee45003b6d708a30065fb16af7b9464
Domen Koder <domen.koder@gmail.com>
2021-12-29 15:26:08 +0100

    code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿static void Random()
+{
+    Random rand = new Random();
+    int randomNumber = rand.Next(1, 7);
+
+    Console.WriteLine(randomNumber);
+    Console.WriteLine("Do you need another number? ENTER yes, BACKSPACE 2-times no");
+}
+
+Random();
+
+while (true)
+{
+    if (Console.ReadKey().Key == ConsoleKey.Enter)
+    {
+        Random();
+    }
+
+    else if (Console.ReadKey().Key == ConsoleKey.Backspace)
+    {
+        Environment.Exit(0);
+    }
+}
+
